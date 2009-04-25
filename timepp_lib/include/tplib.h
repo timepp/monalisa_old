bd37a805b33418377e7153c3bede482333355239
timepp <tongjunhui@live.cn>
2009-04-26 00:28:05 +0800

    minor fix

--- timepp_lib/include/tplib.h
+++ timepp_lib/include/tplib.h
@@ -12,8 +12,8 @@ namespace tp
 	{
 		inline void log_default_config()
 		{
-			tp::log_set_type_names("IVDE");
-			tp::log_add_device(new tp::ld_console, 0xFF, "%H:%M:%S");
+			tp::log_set_type_names(L"IVDE");
+			tp::log_add_device(new tp::ld_console, 0xFF, L"%H:%M:%S");
 		}
 		inline void log_win_error(const wchar_t * prefix)
 		{
