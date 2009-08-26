3a2c480d4481802d34b27e1a67006fc2ec10f10a
timepp <tongjunhui@live.cn>
2009-08-26 20:18:05 +0800

    add ON_RELEASE_3/4

--- timepp_lib/include/auto_release.h
+++ timepp_lib/include/auto_release.h
@@ -31,6 +31,19 @@
 		~CONCAT(s_ol_, __LINE__)() { statement; } \
 	} CONCAT(v_ol_, __LINE__)(var1, var2);
 
+#define ON_LEAVE_3(statement, type1, var1, type2, var2, type3, var3) \
+	struct CONCAT(s_ol_, __LINE__) { \
+		type1 var1; type2 var2; type3 var3; \
+		CONCAT(s_ol_, __LINE__)(type1 v1, type2 v2, type3 v3): var1(v1), var2(v2), var3(v3) {} \
+		~CONCAT(s_ol_, __LINE__)() { statement; } \
+	} CONCAT(v_ol_, __LINE__)(var1, var2, var3);
+
+#define ON_LEAVE_4(statement, type1, var1, type2, var2, type3, var3, type4, var4) \
+	struct CONCAT(s_ol_, __LINE__) { \
+		type1 var1; type2 var2; type3 var3; type4 var4; \
+		CONCAT(s_ol_, __LINE__)(type1 v1, type2 v2, type3 v3, type4 v4): var1(v1), var2(v2), var3(v3), var4(v4) {} \
+		~CONCAT(s_ol_, __LINE__)() { statement; } \
+	} CONCAT(v_ol_, __LINE__)(var1, var2, var3, var4);
 
 namespace tp
 {
